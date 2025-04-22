4ff237b70f81c8ff3492f701704f77cc4bdc5900
Yury Demidov <laikr@rambler.ru>
2025-04-22 22:13:07 +0300

    2025/04/22

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
+
 from utils.config_reader import config
 from utils.core import read_file_excel
 from utils.db_map import Repozitory, Zuzubliks
@@ -21,37 +22,39 @@ bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher(bot)
 
 
-async def data_zuzublik(document: dict) -> dict:
-    file_id = document.file_id
-    file = await bot.get_file(file_id)
-    file_path = file.file_path
-    filename = document.file_name
-    destination = os.path.join(UPLOAD_FOLDER, filename)
-    await bot.download_file(file_path, destination)
-    zuzublik = await read_file_excel(destination)
-    await Repozitory.get_or_create(Zuzubliks, **zuzublik)
-    return zuzublik
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['/upload_a_file']
+    keyboard.add(*buttons)
+    await message.reply('Загрузить файл>>', reply_markup=keyboard)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['upload_a_file'])
 async def send_file(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(keyboard=[['/start']])
     file = open('documents/Лист Microsoft Excel.xlsx', 'rb')
-    answer = await read_file_excel(file)
-    await Repozitory.get_or_create(Zuzubliks, **answer)
-    await message.reply_document(file, reply_markup=keyboard)
+    kwargs = await read_file_excel(file)
+    await Repozitory.get_or_create(Zuzubliks, **kwargs)
+    await message.reply_document(file)
     await message.reply(
-        f'title {answer["title"]} url {answer["url"]} '
-        f'xpath {answer["xpath"]}',
+        f'title {kwargs["title"]} url {kwargs["url"]} '
+        f'xpath {kwargs["xpath"]}',
     )
 
 
 @dp.message_handler(content_types=['document'])
 async def get_docs(message: types.Message):
-    answer = await data_zuzublik(message.document)
+    file_id = message.document.file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    filename = message.document.file_name
+    destination = os.path.join(UPLOAD_FOLDER, filename)
+    await bot.download_file(file_path, destination)
+    kwargs = await read_file_excel(destination)
+    await Repozitory.get_or_create(Zuzubliks, **kwargs)
     await message.reply(
-        f'title {answer["title"]} url {answer["url"]} '
-        f'xpath {answer["xpath"]}',
+        f'title {kwargs["title"]} url {kwargs["url"]} '
+        f'xpath {kwargs["xpath"]}',
     )
 
 
